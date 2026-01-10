18b3edac475afed8cb0f98d79fe8504d1074c849
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-01-10 10:24:46 +0500

    last changes

--- src/app/features/home/shipping-methods/shipping-methods.ts
+++ src/app/features/home/shipping-methods/shipping-methods.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-shipping-methods',
-  imports: [],
+  imports: [Button],
   templateUrl: './shipping-methods.html',
   styleUrl: './shipping-methods.scss'
 })
 export class ShippingMethods {
+  protected cardItems: CardItem[] = [
+    {
+      imageUrl:"../../../../assets/images/truck.jpg",
+      label: "Open Transport"
+    },
+    {
+      imageUrl:"../../../../assets/images/classic-car.jpg",
+      label: "Enclosed and Classic Car Transport"
+    },
+    {
+      imageUrl:"../../../../assets/images/moving.jpg",
+      label: "Seasonal and College Transports"
+    },
+    {
+      imageUrl:"../../../../assets/images/beach.jpg",
+      label: "Hawaii Transport"
+    },
+  ];
+}
 
+export interface CardItem {
+  imageUrl?: string,
+  label?: string;
 }
