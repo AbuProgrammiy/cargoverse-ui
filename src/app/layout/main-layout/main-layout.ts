d9c8a2ce2563fff6c37207bb73e578cc55313c97
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-03-30 07:05:13 +0500

    lates

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBar } from "../../shared/top-bar/top-bar";
+import { Navbar } from "../../shared/components/navbar/navbar";
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     RouterOutlet,
-    TopBar
+    TopBar,
+    Navbar
 ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
