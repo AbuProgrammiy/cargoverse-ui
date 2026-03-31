29b529b1e6a065d1d29db1f7f3aab3d94bf869fa
Abdukholiq NG <abdukholiq23@gmail.com>
2026-03-31 12:40:35 +0500

    leates

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Overview } from "./components/overview/overview";
 import { Welcome } from './components/welcome/welcome';
 
 @Component({
   selector: 'app-home',
   imports: [
-    Welcome
+    Welcome,
+    Overview
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
