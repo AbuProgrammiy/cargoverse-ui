8558227428d0bb87d8a2afaf816e4bee068c75ae
Abdukholiq NG <abdukholiq23@gmail.com>
2026-04-01 15:36:52 +0500

    footer

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "../../shared/components/footer/footer";
 import { Navbar } from "../../shared/components/navbar/navbar";
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     RouterOutlet,
-    Navbar
+    Navbar,
+    Footer
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
