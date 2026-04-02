cf0df4dd53f29d21fdae4bf240220d5d117f822b
Abdukholiq NG <abdukholiq23@gmail.com>
2026-04-02 18:06:40 +0500

    latesrf

--- src/app/features/home/components/submit-form/submit-form.ts
+++ src/app/features/home/components/submit-form/submit-form.ts
@@ -2,13 +2,15 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { InputText } from 'primeng/inputtext';
 import { Select } from 'primeng/select';
+import { Textarea } from 'primeng/textarea';
 
 @Component({
   selector: 'app-submit-form',
   imports: [
     ReactiveFormsModule,
     InputText,
-    Select
+    Select,
+    Textarea
   ],
   templateUrl: './submit-form.html',
   styleUrl: './submit-form.scss'
@@ -22,6 +24,7 @@ export class SubmitForm {
     phoneNumber: this.fb.control<string | null>(null),
     shippingFrom: this.fb.control<string | null>(null),
     shippingTo: this.fb.control<string | null>(null),
+    comment: this.fb.control<string | null>(null),
   });
 
   protected readonly shippingOptions = [
