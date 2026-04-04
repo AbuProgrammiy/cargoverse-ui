c76a113e9445a5be12f9f44440dff18951019591
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-04-04 20:24:08 +0500

    dadsa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { MainLayout } from './layout/main-layout/main-layout';
+import { Admin } from './features/admin/admin';
+import { AdminLogin } from './features/admin/components/admin-login/admin-login';
 
 export const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: MainLayout,
     children: [
       {
-        path: "",
-        component: Home
-      }
-    ]
-  }
+        path: '',
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    component: Admin,
+  },
+  {
+    path: 'admin/login',
+    component: AdminLogin,
+  },
 ];
---
+++ src/app/features/admin/components/admin-login/admin-login.ts
@@ -0,0 +1,38 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputText } from 'primeng/inputtext';
+import { Button } from 'primeng/button';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-admin-login',
+  imports: [InputText, FormsModule, Button],
+  templateUrl: './admin-login.html',
+  styleUrl: './admin-login.scss',
+})
+export class AdminLogin {
+  private readonly router = inject(Router);
+  private readonly messageService = inject(MessageService);
+  protected password = signal<string | null>(null);
+  private readonly PASSWORD = 'root';
+
+  protected submit() {
+    if (this.password() == this.PASSWORD) {
+      this.messageService.add({
+        summary: 'Success',
+        detail: 'Successfully loged in !',
+        severity: 'success',
+      });
+
+      localStorage.setItem('hasAccess', 'true');
+      this.router.navigate(['admin']);
+    } else {
+      this.messageService.add({
+        summary: 'Error',
+        detail: 'Password incorrect !',
+        severity: 'error',
+      });
+    }
+  }
+}
